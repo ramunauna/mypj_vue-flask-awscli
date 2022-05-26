00834c050e9aa4d88b503964f06f4c20bdfc2c9a
Sho Unayama <unayama_sho@cri-me.com>
2022-05-27 02:05:33 +0900

    created header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="header_div">
+        <div class="header_div-logo">
+            <h1>My SPA</h1>
+        </div>
+        <div class="header_div-link">
+            <ul>
+                <li><router-link to="/">Home</router-link></li>
+                <li><router-link to="/">About</router-link></li>
+                <li><router-link to="/">Content</router-link></li>
+            </ul>
+        </div>
+    </header>
+</template>
